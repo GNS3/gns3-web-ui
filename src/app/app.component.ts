e9eb230f57a99d3df93b6398fa816f5c03226d6f
ziajka <dominik.ziajka@gmail.com>
2017-10-31 15:45:38 +0100

    Palette

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import {DomSanitizer} from "@angular/platform-browser";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent implements OnInit {
     constructor(http: Http, iconReg: MatIconRegistry, sanitizer: DomSanitizer) {
