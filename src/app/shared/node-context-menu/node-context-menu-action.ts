2ab96ab32342d6316fe042ae7b43e520c13a1e48
ziajka <dominik.ziajka@gmail.com>
2017-11-30 09:00:23 +0100

    Removed unused files

--- src/app/shared/node-context-menu/node-context-menu-action.ts
+++
@@ -1,8 +0,0 @@
-import {Node} from "../../cartography/shared/models/node.model";
-
-export interface NodeContextMenuAction {
-  icon(node: Node): string;
-  label(node: Node): string;
-  isVisible(node: Node): boolean;
-  onClick(node: Node);
-}
