63b2f2340a72b9f4f94644cbc3514f07610682c0
ziajka <dominik.ziajka@gmail.com>
2017-11-30 13:08:54 +0100

    Design improvements

--- src/app/appliance/appliance-list-dialog/appliance-list-dialog.component.ts
+++ src/app/appliance/appliance-list-dialog/appliance-list-dialog.component.ts
@@ -1,10 +1,18 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Inject, Input, OnInit, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {DataSource} from "@angular/cdk/collections";
 import {Observable} from "rxjs/Observable";
 import {Appliance} from "../../shared/models/appliance";
 import {ApplianceService} from "../../shared/services/appliance.service";
 import {Server} from "../../shared/models/server";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/observable/fromEvent';
 
 
 @Component({
@@ -17,6 +25,8 @@ export class ApplianceListDialogComponent implements OnInit {
   dataSource: ApplianceDataSource;
   displayedColumns = ['name'];
 
+  @ViewChild('filter') filter: ElementRef;
+
   constructor(
     public dialogRef: MatDialogRef<ApplianceListDialogComponent>,
     private applianceService: ApplianceService,
@@ -26,20 +36,36 @@ export class ApplianceListDialogComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource = new ApplianceDataSource(this.server, this.applianceService);
-  }
 
-  onAddClick(): void {
-    this.dialogRef.close();
+    Observable.fromEvent(this.filter.nativeElement, 'keyup')
+      .debounceTime(150)
+      .distinctUntilChanged()
+      .subscribe(() => {
+        if (!this.dataSource) { return; }
+        this.dataSource.filter = this.filter.nativeElement.value;
+      });
+
   }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
 }
 
 
 export class ApplianceDataSource extends DataSource<Appliance> {
+  filterChange = new BehaviorSubject('');
+
+  get filter(): string { return this.filterChange.value; }
+  set filter(filter: string) { this.filterChange.next(filter); }
+
   constructor(private server: Server, private applianceService: ApplianceService) {
     super();
   }
