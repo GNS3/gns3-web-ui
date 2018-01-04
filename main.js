1cc54e8520f4c60f0dcd3ed8b93701c505e16ed8
ziajka <dominik.ziajka@gmail.com>
2018-01-04 12:50:20 +0100

    Icons set

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
