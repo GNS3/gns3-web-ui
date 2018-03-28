9dfbd5268b5a103887d003e05777b250c4d6f9b5
ziajka <dominik@gns3.net>
2018-03-28 13:17:26 +0200

    InRectangleHelper tests

--- src/app/cartography/map/helpers/in-rectangle-helper.ts
+++ src/app/cartography/map/helpers/in-rectangle-helper.ts
@@ -1,6 +1,8 @@
-import {Selectable} from "../../shared/managers/selection-manager";
-import {Rectangle} from "../../shared/models/rectangle";
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
+
+import { Selectable } from "../../shared/managers/selection-manager";
+import { Rectangle } from "../../shared/models/rectangle";
+
 
 @Injectable()
 export class InRectangleHelper {
