db3e700204b201248c0ee3143206e405df8cc556
ziajka <dominik@gns3.net>
2018-07-04 13:44:35 +0200

    Progress error status, Fixes: #146

--- src/app/common/progress/progress.service.spec.ts
+++ src/app/common/progress/progress.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { ProgressService } from './progress.service';
+import { ProgressService, State } from './progress.service';
 
 
 describe('ProgressService', () => {
+  let progressService: ProgressService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ProgressService]
     });
+
+    progressService = TestBed.get(ProgressService);
+
+    spyOn(progressService.state, 'next');
   });
 
-  it('should be created', inject([ProgressService], (service: ProgressService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(progressService).toBeTruthy();
+  });
+
+  it('should propagate event when activated', () => {
+    progressService.activate();
+    expect(progressService.state.next).toHaveBeenCalledWith(new State(true));
+  });
+
+  it('should propagate event when deactivated', () => {
+    progressService.deactivate();
+    expect(progressService.state.next).toHaveBeenCalledWith(new State(false));
+  });
+
+  it('should propagate event on error', () => {
+    const error = new Error();
+    progressService.setError(error);
+    expect(progressService.state.next).toHaveBeenCalledWith(new State(false, error));
+  });
+
+  it('should clear an error', () => {
+    progressService.clear();
+    expect(progressService.state.next).toHaveBeenCalledWith(new State(false, null, true));
+  });
 });
