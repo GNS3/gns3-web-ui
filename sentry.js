da49ac51e6ce980bd7130a45a610aaae33d4146f
ziajka <dominik@gns3.com>
2018-07-20 13:28:49 +0200

    Disable sentry, Ref: #158

--- sentry.js
+++ sentry.js
@@ -1,26 +1,26 @@
-const { init } = require('@sentry/electron');
-const fs = require('fs');
-const { ipcMain } = require('electron');
+// const { init } = require('@sentry/electron');
+// const fs = require('fs');
+// const { ipcMain } = require('electron');
 
-let crashReportsEnabled = true;
-const DSN =
-  'https://cb7b474b2e874afb8e400c47d1452ecc:7876224cbff543d992cb0ac4021962f8@sentry.io/1040940';
+// let crashReportsEnabled = true;
+// const DSN =
+//   'https://cb7b474b2e874afb8e400c47d1452ecc:7876224cbff543d992cb0ac4021962f8@sentry.io/1040940';
 
-const isDev = () => {
-  return fs.existsSync('.git');
-};
+// const isDev = () => {
+//   return fs.existsSync('.git');
+// };
 
-const shouldSendCallback = () => {
-  return !isDev() && crashReportsEnabled;
-};
-
-
-ipcMain.on('settings.changed', function (event, settings) {
-  crashReportsEnabled = settings.crash_reports;
-});
+// const shouldSendCallback = () => {
+//   return !isDev() && crashReportsEnabled;
+// };
 
 
-init({
-  dsn: DSN,
-  shouldSendCallback: shouldSendCallback
-});
+// ipcMain.on('settings.changed', function (event, settings) {
+//   crashReportsEnabled = settings.crash_reports;
+// });
+
+
+// init({
+//   dsn: DSN,
+//   shouldSendCallback: shouldSendCallback
+// });
