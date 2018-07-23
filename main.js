88f61340925eb06b022ef06ebbffe61dd8a9fd0a
ziajka <dominik@gns3.com>
2018-07-23 16:37:09 +0200

    Reenable sentry

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const path = require('path');
 const url = require('url');
 const yargs = require('yargs');
 
-// require('./sentry');
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
