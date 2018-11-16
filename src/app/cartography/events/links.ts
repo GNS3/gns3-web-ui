17b07de620fc904af7abee64dce229856d21bbb7
ziajka <dominik@gns3.net>
2018-11-16 13:49:06 +0100

    Clean unused code

--- src/app/cartography/events/links.ts
+++ src/app/cartography/events/links.ts
@@ -2,15 +2,6 @@ import { MapNode } from "../models/map/map-node";
 import { MapPort } from "../models/map/map-port";
 
 
-// export class LinkCreated {
-//   constructor(
-//       public sourceNode: Node,
-//       public sourcePort: Port,
-//       public targetNode: Node,
-//       public targetPort: Port
-//   ) {}
-// }
-
 export class MapLinkCreated {
   constructor(
       public sourceNode: MapNode,
--- src/app/cartography/events/nodes.ts
+++ src/app/cartography/events/nodes.ts
@@ -7,8 +7,5 @@ class NodeEvent {
     ) {}
 }
 
-export class NodeDragging extends NodeEvent {}
-export class NodeDragged extends NodeEvent {}
-
 export class NodeClicked extends NodeEvent {}
 export class NodeContextMenu extends NodeEvent {}
