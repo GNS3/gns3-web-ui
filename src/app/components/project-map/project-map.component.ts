c14a59d946facdaee29f1c598d3f2beb8f9c96b8
ziajka <dominik@gns3.net>
2018-11-28 10:09:53 +0100

    Tools as public

--- src/app/components/project-map/project-map.component.ts
+++ src/app/components/project-map/project-map.component.ts
@@ -58,7 +58,7 @@ export class ProjectMapComponent implements OnInit, OnDestroy {
 
   private ws: Subject<any>;
 
-  protected tools = {
+  tools = {
     'selection': true,
     'moving': false,
     'draw_link': false
