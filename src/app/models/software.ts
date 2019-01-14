366ed96f28f59dc55d26ae58ebc7049933a3c0d7
ziajka <dominik@gns3.com>
2019-01-14 14:55:18 +0100

    Software model

---
+++ src/app/models/software.ts
@@ -0,0 +1,12 @@
+export type SoftwareType = "web";
+
+export class Software {
+    name: string;
+    locations: string[];
+    type: SoftwareType;
+    resource: string;
+    binary: string;
+    sudo: boolean;
+    installation_arguments: string[];
+    installed: boolean;
+}
