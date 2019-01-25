4ea8c9568f70c9d5526366a4e3311ba1e929043e
ziajka <dominik@gns3.com>
2019-01-25 13:49:34 +0100

    Clean set-variables-in-env.js

--- set-variables-in-env.js
+++ set-variables-in-env.js
@@ -1,14 +1,11 @@
 const yargs = require('yargs');
-// const tmp = require('tmp');
 const fs = require('fs');
-const path = require('path');
 
 const argv = yargs.argv;
 const tempFile = `.temp-var-file.ts`;
 
 if(argv.set) {
     const envFile = argv.set;
-
     console.log(`Backuping up '${envFile}' into '${tempFile}'.`);
     fs.copyFileSync(envFile, tempFile);
     const content = fs.readFileSync(envFile, "utf8");
