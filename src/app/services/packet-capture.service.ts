679a0d02bbca484c30f8cea03ab0cd1ac8d9cf39
Piotr Pekala <piotrpawelpekala@gmail.com>
2019-09-24 04:14:44 -0700

    Support for starting wireshark

--- src/app/services/packet-capture.service.ts
+++ src/app/services/packet-capture.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpServer } from './http-server.service';
 import { Server } from '../models/server';
-import { VpcsTemplate } from '../models/templates/vpcs-template';
-import { Observable } from 'rxjs';
-import { HttpHeaders } from '@angular/common/http';
 import { Project } from '../models/project';
 import { Link } from '../models/link';
 
 @Injectable()
 export class PacketCaptureService {
-    constructor(private httpServer: HttpServer) {}
+    constructor() {}
 
     startCapture(server: Server, project: Project, link: Link, name: string) {
         location.assign(`gns3+pcap://${server.host}:${server.port}?project_id=${project.project_id}&link_id=${link.link_id}&name=${name}`);
