aff1771951271db69bb03d2e6dd47f87783055de
Piotr Pekala <piotrpawelpekala@gmail.com>
2019-10-31 00:22:05 -0700

    Updating settings component & theme.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -245,6 +245,7 @@ import { ScreenshotDialogComponent } from './components/project-map/screenshot-d
 import { ResizableModule } from 'angular-resizable-element';
 import { DragAndDropModule } from 'angular-draggable-droppable';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 if (environment.production) {
   Raven.config('https://b2b1cfd9b043491eb6b566fd8acee358@sentry.io/842726', {
@@ -431,7 +432,8 @@ if (environment.production) {
     ResizableModule,
     DragAndDropModule,
     DragDropModule,
-    MATERIAL_IMPORTS
+    MATERIAL_IMPORTS,
+    OverlayModule
   ],
   providers: [
     SettingsService,
--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { SettingsService } from '../../services/settings.service';
 import { ToasterService } from '../../services/toaster.service';
 import { ConsoleService } from '../../services/settings/console.service';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-settings',
@@ -12,10 +13,13 @@ export class SettingsComponent implements OnInit {
   settings = { ...SettingsService.DEFAULTS };
   consoleCommand: string;
 
+  @HostBinding('class') componentCssClass;
+
   constructor(
     private settingsService: SettingsService,
     private toaster: ToasterService,
-    private consoleService: ConsoleService) {}
+    private consoleService: ConsoleService,
+    public overlayContainer: OverlayContainer) {}
 
   ngOnInit() {
     this.settings = this.settingsService.getAll();
@@ -25,5 +29,11 @@ export class SettingsComponent implements OnInit {
   save() {
     this.settingsService.setAll(this.settings);
     this.toaster.success('Settings have been saved.');
+  }
+
+  onSetTheme(theme) {
+    this.overlayContainer.getContainerElement().classList.remove(this.componentCssClass);
+    this.overlayContainer.getContainerElement().classList.add(theme);
+    this.componentCssClass = theme;
   }
 }
