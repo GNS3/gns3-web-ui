6f6506347738376b2200b60eb87d8f527443dc47
Piotr Pekala <piotrpawelpekala@gmail.com>
2019-11-05 00:31:11 -0800

    Checking if ad is loaded correctly added

--- src/app/components/adbutler/adbutler.component.ts
+++ src/app/components/adbutler/adbutler.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AdButlerResponse } from '../../models/adbutler';
+import { ToasterService } from '../../services/toaster.service';
 
 @Component({
   selector: 'app-adbutler',
@@ -11,16 +12,23 @@ import { AdButlerResponse } from '../../models/adbutler';
 export class AdbutlerComponent implements OnInit {
   @ViewChild('ad', {static: false}) ad: ElementRef;
   @Input() theme: string;
+  @Output() onLoad = new EventEmitter();
   htmlCode: string;
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private toasterService: ToasterService
   ) {}
 
   ngOnInit() {
-    this.httpClient.get('https://servedbyadbutler.com/adserve/;ID=165803;size=0x0;setID=371476;type=json;').subscribe((response: AdButlerResponse) => {
-      this.htmlCode = response.placements.placement_1.body;
-      this.ad.nativeElement.insertAdjacentHTML('beforeend', this.htmlCode);
-    });
+    this.httpClient
+      .get('https://servedbyadbutler.com/adserve/;ID=165803;size=0x0;setID=371476;type=json;').subscribe(
+        response => {
+          this.onLoad.emit(true);
+          this.htmlCode = response['placements'].placement_1.body;
+          this.ad.nativeElement.insertAdjacentHTML('beforeend', this.htmlCode);
+        },
+        error => this.toasterService.error(error)
+      );
   }
 }
