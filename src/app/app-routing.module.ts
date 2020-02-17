6413c2fdece5f4dd7ef47e2908d5a1551fc2147d
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2020-02-17 15:53:41 +0100

    Scroll position restoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -149,7 +149,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
