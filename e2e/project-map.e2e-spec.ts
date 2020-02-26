cf60f1a046f7ca0bf8c574f0121849254ca9f3df
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2020-02-26 19:39:36 +0100

    E2E for project map page

--- e2e/project-map.e2e-spec.ts
+++ e2e/project-map.e2e-spec.ts
@@ -2,15 +2,18 @@ import { ServersPage } from './helpers/server.po';
 import { TestHelper } from './helpers/common.po';
 import { element } from 'protractor';
 import { ProjectsPage } from './helpers/project.po';
+import { ProjectMapPage } from './helpers/project-map.po';
 
 fdescribe('Project map page', () => {
     let serversPage: ServersPage;
     let projectsPage: ProjectsPage;
+    let projectMapPage: ProjectMapPage;
     let helper: TestHelper;
 
     beforeEach(async () => {
         serversPage = new ServersPage();
         projectsPage = new ProjectsPage();
+        projectMapPage = new ProjectMapPage();
         helper = new TestHelper();
 
         serversPage.maximizeWindow();
@@ -23,12 +26,16 @@ fdescribe('Project map page', () => {
         helper.sleep(2000);
     });
 
-    it('user should have possibility to add nodes to map', async () => {
+    fit('user should have possibility to add nodes to map', async () => {
         // arrange
+        projectMapPage.openAddProjectDialog();
+        helper.sleep(2000);
 
         //act
+        projectMapPage.addNode();
+        helper.sleep(2000);
 
         //assert
-        expect(true).toBe(true);
+        expect(await projectMapPage.verifyIfNodeWithLabelExists('PC1')).toBe(true);
     });
 });
