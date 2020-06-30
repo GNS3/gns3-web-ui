9215ab8e87b515d55871fe98d8274ab9eb5f58e7
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2020-06-30 13:41:43 +0200

    downgrade types/node

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { enableDebugTools } from '@angular/platform-browser';
+import '@angular/compiler';
 
 if (environment.production) {
   enableProdMode();
