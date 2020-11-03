5cb3aa051bc000720c21cf8bb0e29a52c771bcff
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2020-11-03 13:46:22 +0100

    Update http-server.service.ts

--- src/app/services/http-server.service.ts
+++ src/app/services/http-server.service.ts
@@ -179,10 +179,8 @@ export class HttpServer {
     if (server.host && server.port) {
       if (server.authorization === 'basic') {
         url = `https://${server.host}:${server.port}/v2${url}`;
-        console.log(url);
       } else {
         url = `http://${server.host}:${server.port}/v2${url}`;
-        console.log(url);
       }
     } else {
       url = `/v2${url}`;
