df6d760444789e70acd6f9a01b3af6b752d85c81
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2021-01-25 15:11:00 +0100

    Update issue-list.tsx

--- src/app/components/help/report-issue/issue-list.tsx
+++ src/app/components/help/report-issue/issue-list.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FunctionComponent, useEffect, useRef, useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 const apiUrl = 'https://api.github.com/repos/GNS3/gns3-web-ui/issues';
 
@@ -23,16 +24,16 @@ class IssueListComponent extends Component<any, any> {
         const { issues } = this.state;
      
         return (
-          <div>
+          <div style={{ justifyContent: 'center', display: 'flex', flex: 1, flexDirection: 'row', flexWrap: 'wrap',  margin: '20px;' }}>
             {issues.map(issue =>
-                <Card key={issue.title} style={{ width: '18rem' }}>
+                <Card key={issue.title} style={{ width: '300px', margin: '20px' }}>
                     <Card.Body>
-                        <Card.Title>{issue.title}</Card.Title>
+                        <Card.Title style={{ color: 'black' }}>{issue.title}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Status: {issue.state}</Card.Subtitle>
-                        <Card.Text>
-                            Description: {issue.body}
+                        <Card.Text style={{ color: 'black', overflowY: 'auto', height: '100px' }}>
+                            {issue.body}
                         </Card.Text>
-                        <Card.Link href="#">{issue.html_url}</Card.Link>
+                        <Card.Link href={issue.html_url} target = "_blank">{issue.html_url}</Card.Link>
                     </Card.Body>
                 </Card>
             )}
