d290daed11b08b331b36c1c796331372002433ae
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2021-05-13 12:03:25 +0200

    Fix for https://github.com/GNS3/gns3-web-ui/issues/1134

--- src/app/services/built-in-templates-configuration.service.ts
+++ src/app/services/built-in-templates-configuration.service.ts
@@ -15,7 +15,7 @@ export class BuiltInTemplatesConfigurationService {
   }
 
   getConsoleTypesForCloudNodes() {
-    return ['telnet', 'none'];
+    return ['telnet', 'vnc', 'spice', 'http', 'https', 'none'];
   }
 
   getCategoriesForEthernetHubs() {
