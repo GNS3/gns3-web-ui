bb2a9632374c8877ad617024d0f2417ec298ca71
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2021-05-18 16:03:53 +0200

    Fix for https://github.com/GNS3/gns3-web-ui/issues/1139

--- src/app/components/project-map/project-map.component.ts
+++ src/app/components/project-map/project-map.component.ts
@@ -538,6 +538,9 @@ export class ProjectMapComponent implements OnInit, OnDestroy {
       return;
     }
 
+    nodeAddedEvent.x = nodeAddedEvent.x / this.mapScaleService.getScale();
+    nodeAddedEvent.y = nodeAddedEvent.y / this.mapScaleService.getScale();
+
     this.progressService.activate();
     this.nodeService
       .createFromTemplate(
