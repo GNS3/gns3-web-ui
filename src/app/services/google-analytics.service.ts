1619c3ec05f435a63a379c644f17247feadfc21d
piotrpekala7 <31202938+piotrpekala7@users.noreply.github.com>
2021-08-24 17:07:47 +0200

    Fix for https://github.com/GNS3/gns3-web-ui/issues/1184

--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { SettingsService } from './settings.service';
 declare var gtag: Function;
 
 @Injectable()
 export class GoogleAnalyticsService {
-  constructor(router: Router) {
+  private settingsService: SettingsService;
+
+  constructor(router: Router, settingsService: SettingsService) {
     if (!environment.production) return;
     router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
+      if (settingsService.getStatisticsSettings() && event instanceof NavigationEnd) {
         gtag('set', 'page', event.url);
         gtag('send', 'pageview');
       }
