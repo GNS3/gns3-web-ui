78da4f0efa2050672334ae48da1ddc931fafcc3f
Rajnikant <rajnikant.lodhi@solarwinds.com>
2022-05-31 10:47:32 +0530

    I have added Remember me tickbox on login component and I resolve an error just before auto re-login

--- src/app/guards/login-guard.ts
+++ src/app/guards/login-guard.ts
@@ -1,51 +1,25 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { LoginService } from '@services/login.service';
-import { ProjectService } from '@services/project.service';
 import { Server } from '../models/server';
 import { ServerService } from '../services/server.service';
 
 @Injectable()
 export class LoginGuard implements CanActivate {
-    constructor(
-        private serverService: ServerService,
-        private loginService: LoginService,
-        private router: Router,
-        private projectService: ProjectService
-    ) { }
+  constructor(private serverService: ServerService, private loginService: LoginService, private router: Router) {}
 
-    async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const server_id = next.paramMap.get('server_id');
-        this.loginService.server_id = server_id
-        let server = await this.serverService.get(parseInt(server_id, 10));
-        
-        try {
-           await this.loginService.getLoggedUser(server)
-        } catch (e) {
-            if (e.status === 401) {
-                // this.test()
-
-                // server.tokenExpired = true;
-                // await this.serverService.update(server)
-                try {
-                //     debugger
-                //     alert('test')
-
-                //     let response = await this.loginService.getLoggedUserRefToken(server)
-                //     server.authToken = response.access_token;
-                //     server.tokenExpired = false;
-                //     await this.serverService.update(server)
-                } catch (e) {
-                    throw e;
-                }
-            }
-        }
-        return this.serverService.get(parseInt(server_id, 10)).then((server: Server) => {
-            if (server.authToken && !server.tokenExpired) {
-                return true
-            }
-            this.router.navigate(['/server', server.id, 'login'], { queryParams: { returnUrl: state.url } });
-        });
-    }
-
+  async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const server_id = next.paramMap.get('server_id');
+    this.loginService.server_id = server_id;
+    let server = await this.serverService.get(parseInt(server_id, 10));
+    try {
+      await this.loginService.getLoggedUser(server);
+    } catch (e) {}
+    return this.serverService.get(parseInt(server_id, 10)).then((server: Server) => {
+      if (server.authToken && !server.tokenExpired) {
+        return true;
+      }
+      this.router.navigate(['/server', server.id, 'login'], { queryParams: { returnUrl: state.url } });
+    });
+  }
 }
--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -1,47 +1,42 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
+import { LoginService } from '@services/login.service';
+import { ServerService } from '@services/server.service';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { ServerService } from '@services/server.service';
-import { LoginService } from '@services/login.service';
 
 @Injectable()
 export class HttpRequestsInterceptor implements HttpInterceptor {
-  constructor(
-    private serverService: ServerService,
-    private loginService: LoginService) { }
+  constructor(private serverService: ServerService, private loginService: LoginService) {}
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(httpRequest).pipe(catchError(err => {
-      if (err.status === 401) {
-        this.call()
-      } else {
-        return throwError(err)
-      }
-    }))
+    return next.handle(httpRequest).pipe(
+      catchError((err) => {
+        if (err.status === 401 || err.status === 403) {
+          this.call();
+        } else {
+          return throwError(err);
+        }
+      })
+    );
   }
+
   async call() {
-    const server_id = this.loginService.server_id
+    const server_id = this.loginService.server_id;
     let server = await this.serverService.get(parseInt(server_id, 10));
     server.tokenExpired = true;
-    await this.serverService.update(server)
+    await this.serverService.update(server);
     try {
-      let response = await this.loginService.getLoggedUserRefToken(server)
+      let response = await this.loginService.getLoggedUserRefToken(server);
       server.authToken = response.access_token;
       server.tokenExpired = false;
-      await this.serverService.update(server)
-      await this.loginService.getLoggedUser(server)
-      this.reloadCurrentRoute()
+      await this.serverService.update(server);
+      await this.loginService.getLoggedUser(server);
+      this.reloadCurrentRoute();
     } catch (e) {
       throw e;
     }
-
-
-
   }
   reloadCurrentRoute() {
-    window.location.reload()
+    location.reload();
   }
-
 }
-
-
