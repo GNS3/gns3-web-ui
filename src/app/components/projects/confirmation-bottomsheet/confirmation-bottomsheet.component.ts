2c3c80bf69b95247b08bfd3809452fc74f6d0ce3
YueGuobin <yueguobin@outlook.com>
2026-03-21 09:18:26 +0800

    feat: migrate ConfirmationBottomSheet component to standalone

--- src/app/components/projects/confirmation-bottomsheet/confirmation-bottomsheet.component.ts
+++ src/app/components/projects/confirmation-bottomsheet/confirmation-bottomsheet.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
-import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { Component, OnInit, Inject, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatBottomSheetRef, MatBottomSheetModule, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
 import { ThemeService } from '@services/theme.service';
 
 @Component({
-  standalone: false,
+  standalone: true,
   selector: 'app-confirmation-bottomsheet',
   templateUrl: 'confirmation-bottomsheet.component.html',
   styleUrls: ['confirmation-bottomsheet.component.scss'],
+  imports: [CommonModule, MatBottomSheetModule, MatButtonModule],
 })
 export class ConfirmationBottomSheetComponent implements OnInit {
+  private bottomSheetRef = inject(MatBottomSheetRef<ConfirmationBottomSheetComponent>);
+  private themeService = inject(ThemeService);
+
   message: string = '';
   isLightThemeEnabled: boolean = false;
 
-  constructor(
-    private bottomSheetRef: MatBottomSheetRef<ConfirmationBottomSheetComponent>,
-    private themeService: ThemeService,
-    @Inject(MAT_BOTTOM_SHEET_DATA) public data: { message: string }
-  ) {}
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { message: string }) {}
 
   ngOnInit() {
     if (this.data && this.data.message) {
