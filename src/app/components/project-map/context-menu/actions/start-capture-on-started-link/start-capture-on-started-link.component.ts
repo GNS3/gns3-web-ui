5683cf84709add03e9b36fbfec73b6a3b3247d6c
YueGuobin <yueguobin@outlook.com>
2026-03-21 09:06:59 +0800

    feat: migrate start-capture-on-started-link action component to standalone

--- src/app/components/project-map/context-menu/actions/start-capture-on-started-link/start-capture-on-started-link.component.ts
+++ src/app/components/project-map/context-menu/actions/start-capture-on-started-link/start-capture-on-started-link.component.ts
@@ -1,20 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { Link } from '@models/link';
 import { Project } from '@models/project';
 import { Controller } from '@models/controller';
 import { PacketCaptureService } from '@services/packet-capture.service';
 
 @Component({
-  standalone: false,
+  standalone: true,
   selector: 'app-start-capture-on-started-link-action',
   templateUrl: './start-capture-on-started-link.component.html',
+  imports: [MatButtonModule, MatIconModule, MatMenuModule],
 })
 export class StartCaptureOnStartedLinkActionComponent {
+  private packetCaptureService = inject(PacketCaptureService);
+
   @Input() controller: Controller;
   @Input() project: Project;
   @Input() link: Link;
-
-  constructor(private packetCaptureService: PacketCaptureService) {}
 
   startCapture() {
     var splittedFileName = this.link.capture_file_name.split('.');
