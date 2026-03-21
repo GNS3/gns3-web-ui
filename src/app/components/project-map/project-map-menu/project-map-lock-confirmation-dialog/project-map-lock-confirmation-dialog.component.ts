0ac962e315f585a5f90f673c3fd7cc36009ad40d
YueGuobin <yueguobin@outlook.com>
2026-03-21 09:23:25 +0800

    feat: migrate ProjectMapLockConfirmationDialog component to standalone

--- src/app/components/project-map/project-map-menu/project-map-lock-confirmation-dialog/project-map-lock-confirmation-dialog.component.ts
+++ src/app/components/project-map/project-map-menu/project-map-lock-confirmation-dialog/project-map-lock-confirmation-dialog.component.ts
@@ -1,21 +1,24 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
-  standalone: false,
+  standalone: true,
   selector: 'app-project-map-lock-confirmation-dialog',
   templateUrl: './project-map-lock-confirmation-dialog.component.html',
-  styleUrls: ['./project-map-lock-confirmation-dialog.component.scss']
+  styleUrls: ['./project-map-lock-confirmation-dialog.component.scss'],
+  imports: [MatDialogModule, MatButtonModule, MatDividerModule],
 })
 export class ProjectMapLockConfirmationDialogComponent implements OnInit {
+  public dialogRef = inject(MatDialogRef<ProjectMapLockConfirmationDialogComponent>);
+
   confirmActionData = {
     actionType: 'Unlock',
     isAction:false
-  };  
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any,
-    public dialogRef: MatDialogRef<ProjectMapLockConfirmationDialogComponent>
-  ) {}
+  };
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 
   ngOnInit(): void {
     this.confirmActionData.actionType = this.data.actionType;
