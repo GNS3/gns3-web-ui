992e456bfa48e0032431386ad27fc82fec89bfae
YueGuobin <yueguobin@outlook.com>
2026-03-22 12:12:21 +0800

    test: migrate installed-software.service.spec.ts to zoneless change detection

--- src/app/services/installed-software.service.spec.ts
+++ src/app/services/installed-software.service.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, provideZonelessChangeDetection } from '@angular/core/testing';
 import { InstalledSoftwareService } from './installed-software.service';
 
 describe('InstalledSoftwareService', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [{ provide: InstalledSoftwareService, useValue: {} }]
+    providers: [provideZonelessChangeDetection(), { provide: InstalledSoftwareService, useValue: {} }]
   }));
 
   it('should be created', () => {
