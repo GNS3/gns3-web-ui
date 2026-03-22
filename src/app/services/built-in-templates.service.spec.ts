a4e9e6e14611b61367ea4f6dd98f0f958c2b1a1f
YueGuobin <yueguobin@outlook.com>
2026-03-22 12:05:21 +0800

    test: migrate built-in-templates.service.spec.ts to zoneless change detection

--- src/app/services/built-in-templates.service.spec.ts
+++ src/app/services/built-in-templates.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { inject, TestBed } from '@angular/core/testing';
+import { inject, TestBed, provideZonelessChangeDetection } from '@angular/core/testing';
 import { environment } from 'environments/environment';
 import { Controller } from '@models/controller';
 import { CloudTemplate } from '@models/templates/cloud-template';
@@ -19,7 +19,7 @@ describe('BuiltInTemplatesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, AppTestingModule],
-      providers: [HttpController, BuiltInTemplatesService],
+      providers: [provideZonelessChangeDetection(), HttpController, BuiltInTemplatesService],
     });
 
     httpClient = TestBed.get(HttpClient);
